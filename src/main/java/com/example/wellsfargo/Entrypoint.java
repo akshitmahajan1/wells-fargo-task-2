04a1ce88c63f9de741fc5465c166fc42f4e99b7d
Akshit Mahajan <akshit.mahajan013@gmail.com>
2025-07-01 01:05:15 +0530

    Added missing Java files

--- src/main/java/com/example/wellsfargo/Entrypoint.java
+++ src/main/java/com/example/wellsfargo/Entrypoint.java
@@ -1,4 +1,4 @@
-package com.example.wellsfargo.counselor;
+package com.example.wellsfargo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/wellsfargo/entity/FinancialAdvisor.java
+++ src/main/java/com/example/wellsfargo/entity/FinancialAdvisor.java
@@ -1,4 +1,4 @@
-package com.example.wellsfargo.counselor.entity;
+package com.example.wellsfargo.entity;
 
 import jakarta.persistence.*;
 import java.util.List;
