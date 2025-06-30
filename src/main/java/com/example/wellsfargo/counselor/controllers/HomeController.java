439c75aad6ae14e851cd384e24595103d3d05c61
Akshit Mahajan <akshit.mahajan013@gmail.com>
2025-07-01 00:56:16 +0530

    Added missing Java files

--- src/main/java/com/example/wellsfargo/counselor/controllers/HomeController.java
+++ src/main/java/com/example/wellsfargo/counselor/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.wellsfargo.counselor.entity;
+package com.example.wellsfargo.counselor.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
